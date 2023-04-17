45da220d75ed3e5c14285c0a36eaf3297558f308
volbot <lucasmcdermitt@icloud.com>
2023-04-16 23:06:24 -0400

    basic textures for gelatin, renderer for items in boiler

--- src/client/java/volbot/beetlebox/render/block/entity/BoilerBlockEntityRenderer.java
+++ src/client/java/volbot/beetlebox/render/block/entity/BoilerBlockEntityRenderer.java
@@ -1,52 +1,121 @@
 package volbot.beetlebox.render.block.entity;
 
+
+
+
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleVariantStorage;
+import net.fabricmc.fabric.impl.client.rendering.fluid.FluidRendererHookContainer;
+import net.fabricmc.fabric.mixin.client.rendering.fluid.FluidRendererMixin;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.model.ModelData;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.model.ModelPartBuilder;
 import net.minecraft.client.model.ModelPartData;
 import net.minecraft.client.model.ModelTransform;
 import net.minecraft.client.model.TexturedModelData;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderLayers;
+import net.minecraft.client.render.block.BlockRenderManager;
+import net.minecraft.client.render.block.FluidRenderer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.chunk.ChunkBuilder.BuiltChunk;
 import net.minecraft.client.render.entity.model.EntityModelPartNames;
-import net.minecraft.client.texture.SpriteAtlasTexture;
-import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.texture.TextureManager;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RotationAxis;
+import net.minecraft.world.LightType;
+import net.minecraft.world.World;
 import volbot.beetlebox.entity.block.BoilerBlockEntity;
 
 public class BoilerBlockEntityRenderer 
 implements BlockEntityRenderer<BoilerBlockEntity>{
 
 	private final ModelPart m;
+	private final BlockRenderManager blockRenderer;
 	
 	public BoilerBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {
+		this.blockRenderer = ctx.getRenderManager();
 		ModelData modelData = new ModelData();
     	ModelPartData modelPartData = modelData.getRoot();
         ModelPartData root = modelPartData.addChild(EntityModelPartNames.ROOT, ModelPartBuilder.create(), ModelTransform.of(0F, 0F, 0F, 0F, 0F, 0F));
-        root.addChild("cube", ModelPartBuilder.create().cuboid("cube", 0.5f, -0.5f, 0.5f, 14f, 14f, 14f), ModelTransform.of(0F, 0F, 0F, 0F, 0F, 0F));
-        m = TexturedModelData.of(modelData, 32, 32).createModel();
+        root.addChild("cube", ModelPartBuilder.create().cuboid("cube", 0.5f, 0.5f, 0.5f, 14f, 14f, 14f).uv(0, 0), ModelTransform.of(0F, 0F, 0F, 0F, 0F, 0F));
+        m = TexturedModelData.of(modelData, 16, 16).createModel();
     }
 	
 	@Override
 	public void render(BoilerBlockEntity boiler, float f, MatrixStack matrices, VertexConsumerProvider vcp, int i, int j) {
-		matrices.push();
+		ItemStack input = boiler.getStack(0);
+		ItemStack output = boiler.getStack(1);
 		SingleVariantStorage<FluidVariant> fluidStorage = boiler.fluidStorage;
 		long amo = fluidStorage.getAmount();
 		long cap = fluidStorage.getCapacity();
-		long beep = (10 * amo / cap);
-		System.out.println(beep);
+		double beep = Math.ceil(10*amo/cap)+1;
+		ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
+		int light = getLightLevel(boiler.getWorld(),boiler.getPos().add(0,1,0));
+		matrices.push();
+		matrices.pop();
+		matrices.push();
+		
+		matrices.pop();
+		matrices.push();
 		if(beep > 0) {
 			matrices.scale(1f, (float)beep / 10f, 1f);
-			VertexConsumer vertexConsumer = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier("entity/bell/bell_body")).getVertexConsumer(vcp, RenderLayer::getEntitySolid);
-			m.render(matrices, vertexConsumer, i, j);
+			VertexConsumer vertexConsumer = vcp.getBuffer(RenderLayer.getEntityTranslucent(new Identifier("beetlebox","textures/block/fluid_boiling.png")));
+			m.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV);
+			if(!input.isEmpty()) {
+				matrices.translate(0.5, 0.75, 0.7);
+		        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(20));
+				itemRenderer.renderItem(input, ModelTransformationMode.GROUND, light, OverlayTexture.DEFAULT_UV, matrices, vcp, boiler.getWorld(), 1);
+				matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-20));
+				matrices.translate(-0.5, -0.75, -0.7);
+		        if(input.getCount()>1) {
+					matrices.translate(0.7, 0.75, 0.4);
+		        	matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(120));
+			        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(20));
+					itemRenderer.renderItem(input, ModelTransformationMode.GROUND, light, OverlayTexture.DEFAULT_UV, matrices, vcp, boiler.getWorld(), 1);
+					matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-20));
+	        		matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-120));
+					matrices.translate(-0.7, -0.75, -0.4);
+	        		if(input.getCount()>2) {
+						matrices.translate(0.3, 0.75, 0.4);
+		        		matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-120));
+				        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(20));
+						itemRenderer.renderItem(input, ModelTransformationMode.GROUND, light, OverlayTexture.DEFAULT_UV, matrices, vcp, boiler.getWorld(), 1);
+						matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-20));
+			        	matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(120));
+						matrices.translate(-0.3, -0.75, -0.4);
+		        	}
+		        }
+			}
+			if(!output.isEmpty()) {
+				matrices.translate(0.5, 0.925, 0.4);
+		        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90));
+				itemRenderer.renderItem(output, ModelTransformationMode.GROUND, light, OverlayTexture.DEFAULT_UV, matrices, vcp, boiler.getWorld(), 5);
+			}
 		}
 		matrices.pop();
+
 	}
+	
+	private int getLightLevel(World world, BlockPos pos) {
+        int bLight = world.getLightLevel(LightType.BLOCK, pos);
+        int sLight = world.getLightLevel(LightType.SKY, pos);
+        return LightmapTextureManager.pack(bLight, sLight);
+    }
 
 }
--- src/main/java/volbot/beetlebox/entity/block/BoilerBlockEntity.java
+++ src/main/java/volbot/beetlebox/entity/block/BoilerBlockEntity.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleVariantStorage;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -37,90 +38,89 @@ import net.minecraft.world.World;
 import volbot.beetlebox.recipe.BoilingRecipe;
 import volbot.beetlebox.registry.BeetleRegistry;
 
-public class BoilerBlockEntity extends BlockEntity implements SidedInventory,
-RecipeUnlocker,
-RecipeInputProvider{
-	
-    protected static final int INPUT_SLOT_INDEX = 0;
-    protected static final int OUTPUT_SLOT_INDEX = 1;
+public class BoilerBlockEntity extends BlockEntity implements SidedInventory, RecipeUnlocker, RecipeInputProvider {
 
-    private static final int[] TOP_SLOTS = new int[]{0};
-    private static final int[] BOTTOM_SLOTS = new int[]{2, 1};
-    private static final int[] SIDE_SLOTS = new int[]{1};
-    protected DefaultedList<ItemStack> inventory = DefaultedList.ofSize(2, ItemStack.EMPTY);
-    int cookTime;
-    int cookTimeTotal;    
-    private final RecipeManager.MatchGetter<Inventory, ? extends BoilingRecipe> matchGetter = 
-    		RecipeManager.createCachedMatchGetter(BeetleRegistry.BOILING_RECIPE_TYPE);
+	protected static final int INPUT_SLOT_INDEX = 0;
+	protected static final int OUTPUT_SLOT_INDEX = 1;
 
-   
-    // This field is going to contain the amount, and the fluid variant (more on that in a bit).
+	private static final int[] TOP_SLOTS = new int[] { 0 };
+	private static final int[] BOTTOM_SLOTS = new int[] { 2, 1 };
+	private static final int[] SIDE_SLOTS = new int[] { 1 };
+	protected DefaultedList<ItemStack> inventory = DefaultedList.ofSize(2, ItemStack.EMPTY);
+	int cookTime;
+	int cookTimeTotal;
+	private final RecipeManager.MatchGetter<Inventory, ? extends BoilingRecipe> matchGetter = RecipeManager
+			.createCachedMatchGetter(BeetleRegistry.BOILING_RECIPE_TYPE);
+
+	// This field is going to contain the amount, and the fluid variant (more on
+	// that in a bit).
 	public final SingleVariantStorage<FluidVariant> fluidStorage = new SingleVariantStorage<>() {
 		@Override
 		protected FluidVariant getBlankVariant() {
 			return FluidVariant.blank();
 		}
- 
+
 		@Override
 		protected long getCapacity(FluidVariant variant) {
 			// Here, you can pick your capacity depending on the fluid variant.
 			// For example, if we want to store 8 buckets of any fluid:
 			return (8 * FluidConstants.BUCKET) / 81; // This will convert it to mB amount to be consistent;
 		}
- 
+
 		@Override
 		protected void onFinalCommit() {
-			// Called after a successful insertion or extraction, markDirty to ensure the new amount and variant will be saved properly.
+			// Called after a successful insertion or extraction, markDirty to ensure the
+			// new amount and variant will be saved properly.
 			markDirty();
-                        if (!world.isClient) {
-                           var buf = PacketByteBufs.create();
-                           buf.writeBlockPos(pos);
-                           fluidStorage.variant.toPacket(buf);
-                           buf.writeLong(BoilerBlockEntity.this.fluidStorage.amount);
-                           PlayerLookup.tracking(BoilerBlockEntity.this).forEach(player -> {
-                               ServerPlayNetworking.send(player, new Identifier("beetlebox","boiler_fluid"), buf);
-                           });
-                        }
+			if (!world.isClient) {
+				var buf = PacketByteBufs.create();
+				buf.writeBlockPos(pos);
+				fluidStorage.variant.toPacket(buf);
+				buf.writeLong(BoilerBlockEntity.this.fluidStorage.amount);
+				PlayerLookup.tracking(BoilerBlockEntity.this).forEach(player -> {
+					ServerPlayNetworking.send(player, new Identifier("beetlebox", "boiler_fluid"), buf);
+				});
+			}
 		}
 	};
 
 	public BoilerBlockEntity(BlockPos pos, BlockState state) {
 		super(BeetleRegistry.BOILER_BLOCK_ENTITY, pos, state);
 	}
-	
+
 	@Override
 	public Packet<ClientPlayPacketListener> toUpdatePacket() {
 		return BlockEntityUpdateS2CPacket.create(this);
 	}
-	
-	protected final PropertyDelegate propertyDelegate = new PropertyDelegate(){
 
-        @Override
-        public int get(int index) {
-            switch (index) {
-                case 0: {
-                    return BoilerBlockEntity.this.cookTime;
-                }
-                case 1: {
-                    return BoilerBlockEntity.this.cookTimeTotal;
-                }
-            }
-            return 0;
-        }
+	protected final PropertyDelegate propertyDelegate = new PropertyDelegate() {
 
-        @Override
-        public void set(int index, int value) {
-            switch (index) {
-                case 0: {
-                    BoilerBlockEntity.this.cookTime = value;
-                    break;
-                }
-                case 1: {
-                	BoilerBlockEntity.this.cookTimeTotal = value;
-                    break;
-                }
-            }
-        }
+		@Override
+		public int get(int index) {
+			switch (index) {
+			case 0: {
+				return BoilerBlockEntity.this.cookTime;
+			}
+			case 1: {
+				return BoilerBlockEntity.this.cookTimeTotal;
+			}
+			}
+			return 0;
+		}
+
+		@Override
+		public void set(int index, int value) {
+			switch (index) {
+			case 0: {
+				BoilerBlockEntity.this.cookTime = value;
+				break;
+			}
+			case 1: {
+				BoilerBlockEntity.this.cookTimeTotal = value;
+				break;
+			}
+			}
+		}
 
 		@Override
 		public int size() {
@@ -128,195 +128,213 @@ RecipeInputProvider{
 		}
 	};
 
-    public static void tick(World world, BlockPos pos, BlockState state, BoilerBlockEntity blockEntity) {
-       	//System.out.println(blockEntity.fluidStorage.amount);
-        boolean bl2 = false;
-        if (!blockEntity.getStack(0).isEmpty()) {
-            BoilingRecipe recipe = (BoilingRecipe)blockEntity.matchGetter.getFirstMatch(blockEntity, world).orElse(null);
-            if(!(recipe.fluid_in.equals(blockEntity.fluidStorage.variant) && recipe.fluid_droplets <= blockEntity.fluidStorage.amount)) {
-            	return;
-            }
-            int i = blockEntity.getMaxCountPerStack();
-            if (BoilerBlockEntity.canAcceptRecipeOutput(world.getRegistryManager(), recipe, blockEntity.inventory, i)) {
-                ++blockEntity.cookTime;
-                if (blockEntity.cookTime == blockEntity.cookTimeTotal) {
-                    blockEntity.cookTime = 0;
-                    blockEntity.cookTimeTotal = BoilerBlockEntity.getCookTime(world, blockEntity);
-                	System.out.println("craft");
-                    BoilerBlockEntity.craftRecipe(world.getRegistryManager(), recipe, blockEntity.inventory, blockEntity.fluidStorage, i);
-                    bl2 = true;
-                }
-            } else {
-                blockEntity.cookTime = 0;
-            }
-        } else if (blockEntity.cookTime > 0) {
-            blockEntity.cookTime = MathHelper.clamp(blockEntity.cookTime - 2, 0, blockEntity.cookTimeTotal);
-        }
-        if (bl2) {
-        	BoilerBlockEntity.markDirty(world, pos, state);
-        }
-    }
-    
-    private static boolean craftRecipe(DynamicRegistryManager registryManager, @Nullable BoilingRecipe recipe, DefaultedList<ItemStack> slots, SingleVariantStorage<FluidVariant> fluidStorage, int count) {
-        if (recipe == null || !BoilerBlockEntity.canAcceptRecipeOutput(registryManager, recipe, slots, count)) {
-            return false;
-        }
-        ItemStack itemStack = slots.get(0);
-        ItemStack itemStack2 = recipe.getOutput(registryManager);
-        ItemStack itemStack3 = slots.get(1);
-        if (itemStack3.isEmpty()) {
-            slots.set(1, itemStack2.copy());
-        } else if (itemStack3.isOf(itemStack2.getItem())) {
-            itemStack3.increment(1);
-        }
+	public static void tick(World world, BlockPos pos, BlockState state, BoilerBlockEntity blockEntity) {
+		// System.out.println(blockEntity.fluidStorage.amount);
+		boolean bl2 = false;
+		if (!blockEntity.getStack(0).isEmpty()) {
+			BoilingRecipe recipe = (BoilingRecipe) blockEntity.matchGetter.getFirstMatch(blockEntity, world)
+					.orElse(null);
+			if (recipe == null) {
+				return;
+			}
+			if (!(recipe.fluid_in.equals(blockEntity.fluidStorage.variant)
+					&& recipe.fluid_droplets <= blockEntity.fluidStorage.amount)) {
+				return;
+			}
+			int i = blockEntity.getMaxCountPerStack();
+			if (BoilerBlockEntity.canAcceptRecipeOutput(world.getRegistryManager(), recipe, blockEntity.inventory, i)) {
+				++blockEntity.cookTime;
+				if (blockEntity.cookTime == blockEntity.cookTimeTotal) {
+					blockEntity.cookTime = 0;
+					blockEntity.cookTimeTotal = BoilerBlockEntity.getCookTime(world, blockEntity);
+					System.out.println("craft");
+					BoilerBlockEntity.craftRecipe(world.getRegistryManager(), recipe, blockEntity.inventory,
+							blockEntity.fluidStorage, i);
+					bl2 = true;
+				}
+			} else {
+				blockEntity.cookTime = 0;
+			}
+		} else if (blockEntity.cookTime > 0) {
+			blockEntity.cookTime = MathHelper.clamp(blockEntity.cookTime - 2, 0, blockEntity.cookTimeTotal);
+		}
+		if (bl2) {
+			BoilerBlockEntity.markDirty(world, pos, state);
+		}
+	}
+
+	private static boolean craftRecipe(DynamicRegistryManager registryManager, @Nullable BoilingRecipe recipe,
+			DefaultedList<ItemStack> slots, SingleVariantStorage<FluidVariant> fluidStorage, int count) {
+		if (recipe == null || !BoilerBlockEntity.canAcceptRecipeOutput(registryManager, recipe, slots, count)) {
+			return false;
+		}
+		ItemStack itemStack = slots.get(0);
+		ItemStack itemStack2 = recipe.getOutput(registryManager);
+		ItemStack itemStack3 = slots.get(1);
+		if (itemStack3.isEmpty()) {
+			slots.set(1, itemStack2.copy());
+		} else if (itemStack3.isOf(itemStack2.getItem())) {
+			itemStack3.increment(1);
+		}
 		Transaction t = Transaction.openOuter();
-        fluidStorage.extract(recipe.fluid_in, recipe.fluid_droplets, t);
-        t.commit();
-        t.close();
-        itemStack.decrement(1);
-        return true;
-    }
-    
-    private static boolean canAcceptRecipeOutput(DynamicRegistryManager registryManager, @Nullable Recipe<?> recipe, DefaultedList<ItemStack> slots, int count) {
-        if (slots.get(0).isEmpty() || recipe == null) {
-            return false;
-        }
-        ItemStack itemStack = recipe.getOutput(registryManager);
-        if (itemStack.isEmpty()) {
-            return false;
-        }
-        ItemStack itemStack2 = slots.get(1);
-        if (itemStack2.isEmpty()) {
-            return true;
-        }
-        if (!itemStack2.isItemEqual(itemStack)) {
-            return false;
-        }
-        if (itemStack2.getCount() < count && itemStack2.getCount() < itemStack2.getMaxCount()) {
-            return true;
-        }
-        return itemStack2.getCount() < itemStack.getMaxCount();
-    }
-    
-    @Override
-    public ItemStack getStack(int slot) {
-        return this.inventory.get(slot);
-    }
+		fluidStorage.extract(recipe.fluid_in, recipe.fluid_droplets, t);
+		t.commit();
+		t.close();
+		itemStack.decrement(1);
+		return true;
+	}
 
-    @Override
-    public ItemStack removeStack(int slot, int amount) {
-        return Inventories.splitStack(this.inventory, slot, amount);
-    }
+	private static boolean canAcceptRecipeOutput(DynamicRegistryManager registryManager, @Nullable Recipe<?> recipe,
+			DefaultedList<ItemStack> slots, int count) {
+		if (slots.get(0).isEmpty() || recipe == null) {
+			return false;
+		}
+		ItemStack itemStack = recipe.getOutput(registryManager);
+		if (itemStack.isEmpty()) {
+			return false;
+		}
+		ItemStack itemStack2 = slots.get(1);
+		if (itemStack2.isEmpty()) {
+			return true;
+		}
+		if (!itemStack2.isItemEqual(itemStack)) {
+			return false;
+		}
+		if (itemStack2.getCount() < count && itemStack2.getCount() < itemStack2.getMaxCount()) {
+			return true;
+		}
+		return itemStack2.getCount() < itemStack.getMaxCount();
+	}
 
-    @Override
-    public ItemStack removeStack(int slot) {
-        return Inventories.removeStack(this.inventory, slot);
-    }
-    
-    @Override
-    public void setStack(int slot, ItemStack stack) {
-        ItemStack itemStack = this.inventory.get(slot);
-        boolean bl = !stack.isEmpty() && stack.isItemEqual(itemStack) && ItemStack.areNbtEqual(stack, itemStack);
-        this.inventory.set(slot, stack);
-        if (stack.getCount() > this.getMaxCountPerStack()) {
-            stack.setCount(this.getMaxCountPerStack());
-        }
-        if (slot == 0 && !bl) {
-            this.cookTimeTotal = BoilerBlockEntity.getCookTime(this.world, this);
-            this.cookTime = 0;
-            this.markDirty();
-        }
-    }
-    
-    private static int getCookTime(World world, BoilerBlockEntity boiler) {
-        return boiler.matchGetter.getFirstMatch(boiler, world).map(AbstractCookingRecipe::getCookTime).orElse(50);
-    }
+	@Override
+	public ItemStack getStack(int slot) {
+		return this.inventory.get(slot);
+	}
+
+	@Override
+	public ItemStack removeStack(int slot, int amount) {
+		ItemStack stack = Inventories.splitStack(this.inventory, slot, amount);
+		this.getWorld().updateListeners(this.getPos(), this.getCachedState(), this.getCachedState(),
+				Block.NOTIFY_LISTENERS);
+		return stack;
+	}
+
+	@Override
+	public ItemStack removeStack(int slot) {
+		ItemStack stack = Inventories.removeStack(this.inventory, slot);
+		this.getWorld().updateListeners(this.getPos(), this.getCachedState(), this.getCachedState(),
+				Block.NOTIFY_LISTENERS);
+		return stack;
+	}
+
+	@Override
+	public void setStack(int slot, ItemStack stack) {
+		ItemStack itemStack = this.inventory.get(slot);
+		boolean bl = !stack.isEmpty() && stack.isItemEqual(itemStack) && ItemStack.areNbtEqual(stack, itemStack);
+		this.inventory.set(slot, stack);
+		if (stack.getCount() > this.getMaxCountPerStack()) {
+			stack.setCount(this.getMaxCountPerStack());
+		}
+		if (slot == 0 && !bl) {
+			this.cookTimeTotal = BoilerBlockEntity.getCookTime(this.world, this);
+			this.cookTime = 0;
+			this.markDirty();
+			this.getWorld().updateListeners(this.getPos(), this.getCachedState(), this.getCachedState(),
+					Block.NOTIFY_LISTENERS);
+		}
+	}
+
+	private static int getCookTime(World world, BoilerBlockEntity boiler) {
+		return boiler.matchGetter.getFirstMatch(boiler, world).map(AbstractCookingRecipe::getCookTime).orElse(50);
+	}
 
 	@Override
 	public int size() {
 		return 2;
 	}
-	
-    @Override
-    public boolean isEmpty() {
-        for (ItemStack itemStack : this.inventory) {
-            if (itemStack.isEmpty()) continue;
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public boolean canPlayerUse(PlayerEntity player) {
-        return Inventory.canPlayerUse(this, player);
-    }
-    @Override
-    public int[] getAvailableSlots(Direction side) {
-        if (side == Direction.DOWN) {
-            return BOTTOM_SLOTS;
-        }
-        if (side == Direction.UP) {
-            return TOP_SLOTS;
-        }
-        return SIDE_SLOTS;
-    }
 
 	@Override
-    public boolean canInsert(int slot, ItemStack stack, @Nullable Direction dir) {
-        return this.isValid(slot, stack);
-    }
+	public boolean isEmpty() {
+		for (ItemStack itemStack : this.inventory) {
+			if (itemStack.isEmpty())
+				continue;
+			return false;
+		}
+		return true;
+	}
 
-    @Override
-    public boolean canExtract(int slot, ItemStack stack, Direction dir) {
-        return true;
-    }
-	
 	@Override
-    public void clear() {
-        this.inventory.clear();
-    }
+	public boolean canPlayerUse(PlayerEntity player) {
+		return Inventory.canPlayerUse(this, player);
+	}
 
-    @Override
-    public void setLastRecipe(@Nullable Recipe<?> recipe) { }
+	@Override
+	public int[] getAvailableSlots(Direction side) {
+		if (side == Direction.DOWN) {
+			return BOTTOM_SLOTS;
+		}
+		if (side == Direction.UP) {
+			return TOP_SLOTS;
+		}
+		return SIDE_SLOTS;
+	}
 
-    @Override
-    @Nullable
-    public Recipe<?> getLastRecipe() {
-        return null;
-    }
-    @Override
-    public void provideRecipeInputs(RecipeMatcher finder) {
-        for (ItemStack itemStack : this.inventory) {
-            finder.addInput(itemStack);
-        }
-    }
-    
+	@Override
+	public boolean canInsert(int slot, ItemStack stack, @Nullable Direction dir) {
+		return this.isValid(slot, stack);
+	}
 
+	@Override
+	public boolean canExtract(int slot, ItemStack stack, Direction dir) {
+		return true;
+	}
+
+	@Override
+	public void clear() {
+		this.inventory.clear();
+		this.getWorld().updateListeners(this.getPos(), this.getCachedState(), this.getCachedState(),
+				Block.NOTIFY_LISTENERS);
+	}
+
+	@Override
+	public void setLastRecipe(@Nullable Recipe<?> recipe) {
+	}
+
+	@Override
+	@Nullable
+	public Recipe<?> getLastRecipe() {
+		return null;
+	}
+
+	@Override
+	public void provideRecipeInputs(RecipeMatcher finder) {
+		for (ItemStack itemStack : this.inventory) {
+			finder.addInput(itemStack);
+		}
+	}
 
 	@Override
 	public NbtCompound toInitialChunkDataNbt() {
 		return createNbt();
 	}
 
-    
-
-    
 	@Override
 	public void writeNbt(NbtCompound tag) {
 		super.writeNbt(tag);
-        tag.putShort("CookTime", (short)this.cookTime);
-        tag.putShort("CookTimeTotal", (short)this.cookTimeTotal);
+		tag.putShort("CookTime", (short) this.cookTime);
+		tag.putShort("CookTimeTotal", (short) this.cookTimeTotal);
 		tag.put("fluidVariant", fluidStorage.variant.toNbt());
 		tag.putLong("amount", fluidStorage.amount);
 		Inventories.writeNbt(tag, this.inventory);
 	}
+
 	@Override
 	public void readNbt(NbtCompound tag) {
 		super.readNbt(tag);
-        this.inventory = DefaultedList.ofSize(this.size(), ItemStack.EMPTY);
-        Inventories.readNbt(tag, this.inventory);
-        this.cookTime = tag.getShort("CookTime");
-        this.cookTimeTotal = tag.getShort("CookTimeTotal");
+		this.inventory = DefaultedList.ofSize(this.size(), ItemStack.EMPTY);
+		Inventories.readNbt(tag, this.inventory);
+		this.cookTime = tag.getShort("CookTime");
+		this.cookTimeTotal = tag.getShort("CookTimeTotal");
 		fluidStorage.variant = FluidVariant.fromNbt(tag.getCompound("fluidVariant"));
 		fluidStorage.amount = tag.getLong("amount");
 	}
