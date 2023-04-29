91df73cac499ec43d14c33abc005b796fd1a07fa
volbot <lucasmcdermitt@icloud.com>
2023-04-29 00:04:30 -0400

    1.0.21

--- src/main/java/volbot/beetlebox/client/render/armor/BeetleArmorEntityModel.java
+++ src/main/java/volbot/beetlebox/client/render/armor/BeetleArmorEntityModel.java
@@ -3,7 +3,6 @@ package volbot.beetlebox.client.render.armor;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
-import net.minecraft.client.render.entity.model.PlayerEntityModel;
 
 public abstract class BeetleArmorEntityModel<T extends LivingEntity> extends BipedEntityModel<T>{
 
