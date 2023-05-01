4feb1ad484effd2dccad627d33debdea6c92cef7
volbot <lucasmcdermitt@icloud.com>
2023-05-01 19:44:59 -0400

    Revert "some  of 1.0.3 leaked in here"
    This reverts commit 1955cea3818a13f0ef69bbc50cc8f9bdf4d5a322.

---
+++ src/main/java/volbot/beetlebox/item/BeetleJelly.java
@@ -0,0 +1,11 @@
+package volbot.beetlebox.item;
+
+import net.minecraft.item.Item;
+
+public class BeetleJelly extends Item{
+
+	public BeetleJelly(Settings settings) {
+		super(settings);
+	}
+	
+}
